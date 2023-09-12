e608471d8f75958a31f1c681922b97318ac7e8e4
edmundliman <edmundliman30@gmail.com>
2023-09-12 17:44:28 +1000

    Implemented naive binomial algorithm

--- src/RouletteGame.py
+++ src/RouletteGame.py
@@ -1,3 +1,5 @@
+import Binomial
+
 class RouletteGame:
     """
     A class used to represent an game of roulette
@@ -62,17 +64,42 @@ class RouletteGame:
     on deciding how to place the bets. Considers next roll to be independent of
     the previous roll. 
 
-    Uses n = len(__global_history) for __global_history and n = 15 for
-    __last_fourteen_history
+    Uses n = len(__global_history) + 1 for __global_history and n = 15 for
+    __last_fourteen_history, assuming we know outcome for the first n - 1 trials.
 
     Then, finds the pmf by considering P(X = len(classification) + 1) where
     x ~ Bi(n, P(classification)). The probability of the next roll being in the
     classification by considering number of successes in sample size n.
 
     This method does not consider the drift factor based on the hand of the dealer.
+
+    Logical explanation:
+    In a n sample size, what is the probability of hitting x + 1 successes
+    Naivety: It will average out to equal probabilities.
     """
-    def naive_binomial(self, list_of_classified):
-        return 0
+    def naive_binomial(self, list_of_classification, list_of_classified, faces):
+        probability_list = []
+        no_choices = len(list_of_classification)
+        next_prob_global = []
+        next_prob_fourteen = []
+
+        for i in range(no_choices):
+            probability_list.append(len(list_of_classification[i]) / len(faces))
+
+        for i in range(no_choices):
+            # Using global
+            x = len(list_of_classified[0][i]) + 1
+            n = len(self.__global_history) + 1
+            p = probability_list[i]
+            next_prob_global.append(Binomial.dbinom(x, n, p))
+
+            # Using last fourteen
+            x = len(list_of_classified[1][i]) + 1
+            n = len(self.__last_fourteen_history) + 1
+            p = probability_list[i]
+            next_prob_fourteen.append(Binomial.dbinom(x, n, p))
+
+        return [next_prob_global, next_prob_fourteen]
     
     """
     This method finds p, the probaility of the next roll hitting the classification
